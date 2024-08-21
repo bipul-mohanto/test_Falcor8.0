895262a01233d0c60cbcd3b589453d3b0a234d68
Bipul Mohanto <bipul.mohanto@yahoo.com>
2024-08-21 17:17:39 +0300

    Falcor 7.0-> Updated to Falcor 8.0
    nvapi: 560
    optix: 8.0.0

--- external/fmt/include/fmt/format.h
+++ external/fmt/include/fmt/format.h
@@ -554,8 +554,7 @@ inline auto get_data(Container& c) -> typename Container::value_type* {
   return c.data();
 }
 
-//#if defined(_SECURE_SCL) && _SECURE_SCL //bm to disable warning as error during debug build
-#if 0
+#if defined(_SECURE_SCL) && _SECURE_SCL
 // Make a checked iterator to avoid MSVC warnings.
 template <typename T> using checked_ptr = stdext::checked_array_iterator<T*>;
 template <typename T>
